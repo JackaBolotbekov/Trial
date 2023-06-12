124c53e900307614c8ec7fc8105b524e88adf81c
JackaBolotbekov <jackabolotbekow@gmail.com>
2023-06-12 11:50:14 +0600

    Initial commit

---
+++ app/src/main/java/com/example/trial/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.trial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.edit_text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=" + editText.getText()));
+                startActivity(intent);
+            }
+        });
+    }
+}
